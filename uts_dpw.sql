fe79a141898023d02360b4d48028210239cb5f18
febriyani <90696202+fikrizulfikarb@users.noreply.github.com>
2022-01-10 23:38:02 +0700

    Update uts_dpw.sql

--- uts_dpw.sql
+++ uts_dpw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 26, 2021 at 04:05 AM
+-- Generation Time: Jan 10, 2022 at 05:36 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -43,8 +43,9 @@ CREATE TABLE `article` (
 
 INSERT INTO `article` (`id`, `id_user`, `title`, `write`, `description`, `created_at`, `updated_at`) VALUES
 (24, 8, '1. Nasi Pecel', 'fikrizulfikarb', 'Nasi pecel adalah hidangan khas Indonesia dari Jawa berupa nasi yang disajikan dengan pecel. Sayuran yang dipakai biasanya kangkung, kacang panjang, daun singkong, dan daun pepaya. Di Jawa Timur, sering ditambahkan kembang turi. Rasanya lebih nikmat jika dimakan dengan tempe goreng dan cracker tradisional yang biasa disebut peyek.', '2021-12-11 22:44:25', '2021-12-11 22:44:25'),
-(25, 8, '2. Rendang', 'fikrizulfikarb', 'Rendang atau randang adalah masakan daging bercita rasa pedas yang menggunakan campuran berbagai bumbu dan rempah-rempah. Masakan ini dihasilkan dari proses memasak yang dipanaskan berulang-ulang dengan santan kelapa.Proses memasaknya memakan waktu berjam-jam (biasanya sekitar empat jam) hingga kering dan berwarna hitam pekat.Dalam suhu ruangan, rendang dapat bertahan hingga berminggu-minggu. Rendang yang dimasak dalam waktu lebih singkat dan santannya belum mengering serta berwarna coklat terang keemasan disebut kalio.Rendang dapat ditemukan di Rumah Makan Padang di seluruh dunia.Masakan ini populer di kalangan masyarakat Indonesia dan negara-negara di Asia Tenggara, seperti Malaysia, Singapura, Brunei, Filipina, dan Thailand. Di daerah asalnya, Minangkabau, rendang disajikan dalam berbagai upacara adat dan acara istimewa.Meskipun rendang merupakan masakan tradisional Minangkabau secara umum, masing-masing daerah di sana memiliki teknik memasak dan penggunaan bumbu yang berbeda.Pada 2011, rendang dinobatkan sebagai hidangan peringkat pertama dalam daftar World\'s 50 Most Delicious Foods (50 Hidangan Terlezat Dunia) yang digelar oleh CNN International. Keren, ya!', '2021-12-12 06:36:36', '2021-12-12 06:36:36'),
-(26, 8, '3. Kerak telor', 'fikrizulfikarb', 'Kerak telur adalah makanan asli daerah Jakarta, dengan bahan-bahan beras ketan putih, telur ayam atau bebek, ebi yang disangrai kering, ditambah bawang merah goreng, lalu diberi bumbu yang dihaluskan berupa kelapa sangrai, cabai merah, kencur, jahe, merica butiran, garam dan gula pasir.\r\n\r\nPada proses penyajiannya, kerak telor yang sudah jadi tadi disajikan di atas piring. Kemudian ditaburi dengan bawang merah dan parutan kelapa yang sudah di sangrai.\r\nPada proses pembuatan, kerak telor ini tidak menggunakan kompor untuk memasaknya, namun menggunakan anglo dan arang. Karena apabila menggunakan kompor, keraknya akan mudah gosong.\r\nKeanekaragaman makanan khas Indonesia ini pastinya bikin bangga kan, sama nih kayak Djarum 76 yang bangga akan keanekaragaman Indonesia salah satunya makanannya dengan membuat campaign #IndonesiaAdalahKita dalam rangka Sumpah Pemuda 2020. Ada juga video commercial tentang #IndonesiaAdalahKita yang pastinya keren banget. Lihat videonya dibawah ini yuk!', '2021-12-12 06:38:04', '2021-12-12 06:38:04');
+(25, 8, '2. Rendang', 'fikrizulfikarb', 'Rendang atau randang adalah masakan daging bercita rasa pedas yang menggunakan campuran berbagai bumbu dan rempah-rempah. Masakan ini dihasilkan dari proses memasak yang dipanaskan berulang-ulang dengan santan kelapa.Proses memasaknya memakan waktu berjam-jam (biasanya sekitar empat jam) hingga kering dan berwarna hitam pekat.Dalam suhu ruangan, rendang dapat bertahan hingga berminggu-minggu. Rendang yang dimasak dalam waktu lebih singkat dan santannya belum mengering serta berwarna coklat terang keemasan disebut kalio.Rendang dapat ditemukan di Rumah Makan Padang di seluruh dunia.Masakan ini populer di kalangan masyarakat Indonesia dan negara-negara di Asia Tenggara, seperti Malaysia, Singapura, Brunei, Filipina, dan Thailand.', '2021-12-12 06:36:36', '2022-01-09 09:54:57'),
+(28, 8, 'Rawon', 'fikrizulfikarb', 'Rawon adalah makan khas dari Kota Surabaya yang mirip dengan sup daging namun memiliki kuah hitam pekat. Meskipun kuahnya tidak seperti sup pada umumnya, rawon memiliki cita rasa yang khas dan selalu dirindukan oleh penikmat masakan Jawa timuran.\r\n\r\nWarna hitam pada rawon disebabkan oleh pemakaian kluwek pada kuahnya. Buah kluwek adalah buah berwarna hitam yang memberikan warna serta citarasa yang unik pada masakan.\r\n\r\nRawon disajikan bersama potongan daging sapi, rebusan kecambah dan telur asin serta kerupuk udang. Jangan lupa tambahkan perasan jeruk nipis dan sambal untuk menambah kesegaran saat menyantap hidangan yang satu ini. Tertarik membuatnya? Donwload saja di e-book Gramedia Digital agar Grameds terampil memasak masakan nusantara yang nikmat rasanya.', '2022-01-09 09:55:36', '2022-01-09 09:55:36'),
+(29, 10, 'Gado-Gado', 'mutiara', 'Gado-gado adalah makanan khas dari Jakarta yang menyehatkan karena terdiri dari rebusan sayuran seperti kangkung, wortel, labu siam, selada, timun dan irisan tempe yang disiram dengan saus kacang yang gurih.\r\n\r\nDi Jawa Barat Gado-gado disebut dengan Lotek. Serupa bentuknya namun tak sama citarasanya. Lotek memakai bumbu kacang, bawang putih, kencur, dan asam jawa sehingga memiliki bau yang lebih kuat dan lebih manis dibandingkan Gado-gado.\r\n\r\nGado-gado dapat disantap dengan lontong atau nasi putih. Harga Gado-gado juga cukup terjangkau, seporsi Gado-gado dibanderol sekitar Rp 10.000 – Rp 15.000 saja.', '2022-01-09 09:58:39', '2022-01-09 09:58:39');
 
 -- --------------------------------------------------------
 
@@ -67,7 +68,7 @@ CREATE TABLE `coment` (
 INSERT INTO `coment` (`id`, `isi`, `nama`, `created_at`, `updated_at`) VALUES
 (18, 'req makanan khas padang dong', 'mega', '2021-12-12 02:14:30', '2021-12-12 02:14:30'),
 (19, 'makanan khas betawi dong', 'elly', '2021-12-12 06:37:03', '2021-12-12 06:37:03'),
-(21, 'makanan khas bali bang', 'lola reghita', '2021-12-24 10:11:49', '2021-12-24 10:11:49');
+(22, 'no komen', 'tiaraaa', '2022-01-09 09:56:05', '2022-01-09 09:56:05');
 
 -- --------------------------------------------------------
 
@@ -92,7 +93,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `username`, `email`, `nama`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (8, 'fikrizulfikarb', 'fikri.zb10@gmail.com', 'fikri', '$2y$10$/mO1ld9hH0o.X7/6iowzLu3PGyI9BoB8Wr.RCCWVYF3iPNIvruAtK', NULL, '2021-12-11 22:09:30', '2021-12-11 22:09:30'),
-(9, 'megabolot', 'megapratiwi17@gmail.com', 'mega pratiwi', '$2y$10$OOl8iz2WJo5fcINF.cFH0.lLC4KrP.9s6.mTzvjqNo3bT14mYdefu', NULL, '2021-12-24 10:07:29', '2021-12-24 10:07:29');
+(10, 'mutiara', 'mutiara@gmail.com', 'mutia', '$2y$10$JmjgqxIp71524gtVhkfNTu7zcc7MK3Rp3ThsTkg9FJghWZS.r7U9u', NULL, '2022-01-09 09:57:22', '2022-01-09 09:57:22');
 
 -- --------------------------------------------------------
 
@@ -113,12 +114,8 @@ CREATE TABLE `user_detail` (
 --
 
 INSERT INTO `user_detail` (`id`, `id_user`, `no_handphone`, `created_at`, `updated_at`) VALUES
-(1, 3, '089689203086', '2021-11-13 22:47:36', '2021-11-13 22:47:36'),
-(2, 4, '089689203086', '2021-11-15 05:38:12', '2021-11-15 05:38:12'),
-(4, 6, NULL, '2021-11-16 23:42:47', '2021-11-16 23:42:47'),
-(5, 7, '089689203086', '2021-11-16 23:44:43', '2021-11-16 23:44:43'),
 (6, 8, '081290034449', '2021-12-11 22:09:30', '2021-12-11 22:09:30'),
-(7, 9, '081290033449', '2021-12-24 10:07:29', '2021-12-24 10:07:29');
+(8, 10, '081290034449', '2022-01-09 09:57:22', '2022-01-09 09:57:22');
 
 --
 -- Indexes for dumped tables
@@ -156,25 +153,25 @@ ALTER TABLE `user_detail`
 -- AUTO_INCREMENT for table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `coment`
 --
 ALTER TABLE `coment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user_detail`
 --
 ALTER TABLE `user_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
